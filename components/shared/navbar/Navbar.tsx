600adb81b0ff6274f5a6ec989988f7e4f04b0b4c
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-09-21 02:12:25 +0300

    add

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function Navbar() {
+async function Navbar() {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href={"/"} className="flex items-center gap-1">
@@ -22,9 +22,9 @@ function Navbar() {
       <p>globalsearch</p>
       <div className="flex">
         theme change
-        {/* <SignedIn> */}
-        <UserButton />
-        {/* </SignedIn> */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
