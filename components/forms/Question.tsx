07b2f61f271aa82d00a1c1e1bc519d82075be125
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-10-04 18:34:13 +0300

    Update Question.tsx

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -13,9 +13,11 @@ import { Input } from "@/components/ui/input";
 import { QuestionsSchema } from "@/lib/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Editor } from '@tinymce/tinymce-react';
-import { useRef } from 'react';
+import Image from "next/image";
+import React, { useRef } from 'react';
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Badge } from "../ui/badge";
 
 function Question() {
     const editorRef = useRef(null);
@@ -35,6 +37,38 @@ function Question() {
         // ✅ This will be type-safe and validated.
         console.log(values)
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function handleonKeyDown(e: React.KeyboardEvent<HTMLInputElement>, field: any) {
+        if (e.key === "Enter" && field.name === "tags") {
+            e.preventDefault()
+            const tagInput = e.target as HTMLInputElement
+            const tagValue = tagInput.value
+
+            if (tagValue !== "") {
+                if (tagValue.length > 15) {
+                    return form.setError("tags", {
+                        type: "required",
+                        message: "Tag must be less than 15 characters"
+                    })
+                }
+                if (!field.value.includes(tagValue)) {
+                    form.setValue("tags", [...field.value, tagValue])
+                    tagInput.value = ""
+                    form.clearErrors("tags")
+                }
+            } else {
+                form.trigger()
+            }
+        }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function handleRemoveTag(tag: string, field: any) {
+        const newTags = field.value.filter((item: string) => item !== tag)
+        form.setValue("tags", newTags)
+    }
+
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}
@@ -53,7 +87,8 @@ function Question() {
                                 />
                             </FormControl>
                             <FormDescription className="body-regular mt-2.5 text-light-500">
-                                Be specific and imagine you’re asking a question to another person.                            </FormDescription>
+                                Be specific and imagine you’re asking a question to another person.
+                            </FormDescription>
                             <FormMessage className="text-red-500" />
                         </FormItem>
                     )}
@@ -62,27 +97,30 @@ function Question() {
                 <FormField
                     control={form.control}
                     name="explanation"
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
                     render={({ field }) => (
                         <FormItem className="flex w-full flex-col gap-3">
                             <FormLabel className="paragraph-semibold text-dark400_light800 ">Detailed explanation of your problem<span className="text-primary-500">*</span></FormLabel>
                             <FormControl className="mt-3.5">
                                 <Editor
-                                    apiKey='your-api-key'
-                                    onInit={(_evt, editor) => editorRef.current = editor}
-                                    initialValue="<p>This is the initial content of the editor.</p>"
+                                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                                    onInit={(_evt, editor) => {
+                                        // @ts-expect-error just ignore
+                                        editorRef.current = editor
+                                    }}
+                                    initialValue=""
                                     init={{
-                                        height: 500,
+                                        height: 350,
                                         menubar: false,
                                         plugins: [
                                             'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-                                            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                                            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+                                            'anchor', 'searchreplace', 'visualblocks', 'codesample', 'fullscreen',
+                                            'insertdatetime', 'media', 'table'
                                         ],
                                         toolbar: 'undo redo | blocks | ' +
-                                            'bold italic forecolor | alignleft aligncenter ' +
-                                            'alignright alignjustify | bullist numlist outdent indent | ' +
-                                            'removeformat | help',
-                                        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                                            'codesample | bold italic forecolor | alignleft aligncenter ' +
+                                            'alignright alignjustify | bullist numlist',
+                                        content_style: 'body { font-family:Inter; font-size:16px }'
                                     }}
                                 />
                             </FormControl>
@@ -102,11 +140,33 @@ function Question() {
                         <FormItem className="flex w-full flex-col">
                             <FormLabel className="paragraph-semibold text-dark400_light800 ">Tags <span className="text-primary-500">*</span></FormLabel>
                             <FormControl className="mt-3.5">
-                                <Input
-                                    placeholder="Add tags..."
-                                    className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 min-h-[56px] border"
-                                    {...field}
-                                />
+                                <>
+                                    <Input
+                                        placeholder="Add tags..."
+                                        className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 min-h-[56px] border"
+                                        onKeyDown={(e) => handleonKeyDown(e, field)}
+                                    />
+                                    {field.value.length > 0 && (
+                                        <div className="flex-start mt-2.5 gap-2.5">
+                                            {field.value.map(tag => (
+                                                <Badge
+                                                    key={tag}
+                                                    className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                                                >
+                                                    {tag}
+                                                    <Image
+                                                        src="/assets/icons/close.svg"
+                                                        alt="delete tag icon"
+                                                        width={12}
+                                                        height={12}
+                                                        className="cursor-pointer object-contain invert-0 dark:invert"
+                                                        onClick={() => handleRemoveTag(tag, field)}
+                                                    />
+                                                </Badge>
+                                            ))}
+                                        </div>
+                                    )}
+                                </>
                             </FormControl>
                             <FormDescription className="body-regular mt-2.5 text-light-500">
                                 Add up to 5 tags to describe what your question is about. Start typing to see suggestions.
