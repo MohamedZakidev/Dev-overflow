1afddaf18a9c68e56d4439f6f42c82852b6581f4
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-10-06 11:19:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable} grid min-h-full place-content-center`}>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <ThemeProvider>{children}</ThemeProvider>
         </body>
       </html>
