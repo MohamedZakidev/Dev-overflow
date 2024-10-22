72b008fb33f47018166c9a6e75beead8c8a7ccf4
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-10-23 01:43:02 +0300

    add

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,10 +6,12 @@ import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import { getQuestions } from "@/lib/actions/question.action";
+import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 
-export default async function Home() {
-  const result = await getQuestions({})
+export default async function Home({ searchParams }: SearchParamsProps) {
+  console.log(searchParams)
+  const result = await getQuestions({ searchQuery: searchParams.q })
 
   return (
     <>
