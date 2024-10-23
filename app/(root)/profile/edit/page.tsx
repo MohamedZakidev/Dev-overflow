b9762e91b0cdfa714f8bf90b7ebc0ec9873f2915
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-10-23 12:51:17 +0300

    Update page.tsx

--- app/(root)/profile/edit/page.tsx
+++ app/(root)/profile/edit/page.tsx
@@ -1,3 +1,4 @@
+import ProfileForm from "@/components/forms/ProfileForm"
 import { getUserById } from "@/lib/actions/user.action"
 import { auth } from "@clerk/nextjs/server"
 
@@ -11,16 +12,15 @@ async function EditProfile() {
     const mongoUser = await getUserById({ userId })
 
     return (
-        <div>edite page</div>
-        // <>
-        //     <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
-        //     <div className="mt-9">
-        //         <ProfileForm
-        //             clerkId={userId}
-        //             user={JSON.stringify(mongoUser)}
-        //         />
-        //     </div>
-        // </>
+        <>
+            <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
+            <div className="mt-9">
+                <ProfileForm
+                    clerkId={userId}
+                    user={JSON.stringify(mongoUser)}
+                />
+            </div>
+        </>
     )
 }
 
