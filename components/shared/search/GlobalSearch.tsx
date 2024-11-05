78ab9c242e6a64c668c4bd578f51cd780d79ecc6
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-11-05 03:48:15 +0200

    Update GlobalSearch.tsx

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,7 +1,40 @@
+"use client"
 import { Input } from "@/components/ui/input"
+import { formURLQuery, removeQueryParamater } from "@/lib/utils"
 import Image from "next/image"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
 
 function GlobalSearch() {
+    const pathname = usePathname()
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const query = searchParams.get("global")
+
+    const [search, setSearch] = useState(query || "")
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            if (search) {
+                const newURL = formURLQuery({
+                    queryParamaters: searchParams.toString(), // Query paramaters or strings // q=someting
+                    key: "global",
+                    value: search // search is the query the user type
+                })
+                router.push(newURL, { scroll: false })
+            } else if (query) {
+                const newURL = removeQueryParamater({
+                    queryParamaters: searchParams.toString(), // Query paramaters or strings // q=someting
+                    keysToRemove: ["global"],
+                })
+                router.push(newURL, { scroll: false })
+            }
+
+        }, 300)
+        return () => clearTimeout(delayDebounceFn)
+    }, [search, searchParams, router, pathname])
+
+
     return (
         <div className=" relative w-full max-w-[600px] max-lg:hidden ">
             <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
@@ -15,7 +48,8 @@ function GlobalSearch() {
                 <Input
                     type="text"
                     placeholder="Search globally"
-                    // value=""
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                     className="paragraph-regular no-focus dark:dark-gradient border-none bg-light-800 shadow-none outline-none"
                 />
             </div>
